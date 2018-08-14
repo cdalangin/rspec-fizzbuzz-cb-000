24add6a73f2cf130e0c26aee32c625410f0e90d2
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-14 02:10:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(fizz_i)
+def fizzbuzz(i)
   if i % 3 == 0
     puts "Fizz"
-  elsif i % 5 == 0
+  if i % 5 == 0
     puts "Buzz"
-  elsif i % 5 && 3 == 0
+  if i % 5 && 3 == 0
     puts "FizzBuzz"
-  else
-    puts "nil"
   end
 end
