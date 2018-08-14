ee9507e9da64065c5f7fc988c5136d16e3bb6ad5
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-14 02:44:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(i)
-  if i % 3 && 5 == 0
+  if i % 3 == 0 && i % 5 == 0
     "FizzBuzz"
   elsif i % 5 == 0 && !(i % 3 == 0)
     "Buzz"
